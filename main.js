1ab7d0995e9a1125055542f014a4c783c0e5532d
Henzer Garcia <henzer.g.09@gmail.com>
2020-01-02 21:39:00 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, dialog, globalShortcut } = require('electron');
+let win;
+
+function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+
+    // and load the index.html of the app.
+    win.loadFile('index.html');
+}
+app.on('ready', () => {
+    win = new BrowserWindow({ width: 800, height: 600, frame: false });
+    win.loadFile('index.html');
+    win.show()
+});
+
+app.on('will-quit', () => {
+    globalShortcut.unregisterAll();
+});
