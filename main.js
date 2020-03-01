380801fb16ad1aa46037f78d44dce8d67a4364a5
Henzer Garcia <henzer.g.09@gmail.com>
2020-02-29 22:33:31 -0600

    Testing GPIO pins

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, dialog, globalShortcut, ipcMain } = require('electro
 const { readMusic } = require('./files-reader');
 const Store = require('electron-store');
 const store = new Store();
+const Gpio = require('onoff').Gpio;
 
 let win;
 let player;
@@ -52,3 +53,23 @@ ipcMain.on('showRockola', (event) => {
 app.on('will-quit', () => {
     globalShortcut.unregisterAll();
 });
+
+var pushButton = new Gpio(2, 'in', 'both');
+
+pushButton.watch(function (err, value) {
+    if (err) {
+        console.error('There was an error', err);
+        return;
+    }
+    console.log('Se presiono el pin 2');
+});
+
+
+function unexportOnClose() { //function to run when exiting program
+    LED.writeSync(0); // Turn LED off
+    LED.unexport(); // Unexport LED GPIO to free resources
+    pushButton.unexport(); // Unexport Button GPIO to free resources
+};
+
+process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
+
